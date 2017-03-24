a056cb119d21c097c4c483cadf8c67846015fea7
Daniel Parry <parryd424@gmail.com>
2017-03-24 11:29:32 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+  count = 0
+  returnable_buddy = []
+  while count < collection.length
+    if yield(collection[count])
+      returnable_buddy << collection[count]
+    end
+    count += 1
+  end
+  returnable_buddy
 end
